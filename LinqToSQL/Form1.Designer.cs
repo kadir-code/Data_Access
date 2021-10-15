fe7ab0952626395529fee855637710519bc9a2b7
kadir-code <65715423+xkadir@users.noreply.github.com>
2021-10-16 00:27:28 +0300

    Link TO Sql

--- LinqToSQL/Form1.Designer.cs
+++ LinqToSQL/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace LinqToSQL
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExecute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(802, 240);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(300, 335);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(124, 35);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExecute;
     }
 }
 
--- LinqToSQL/Form1.cs
+++ LinqToSQL/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using LinqToSQL.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.SqlServer;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,111 @@ namespace LinqToSQL
 {
     public partial class Form1 : Form
     {
+        NorthwindEntities db = new NorthwindEntities();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+
+            //var employees = from x in db.Employees
+            //                select new
+            //                {
+            //                    x.FirstName,
+            //                    x.LastName,
+            //                    x.Title,
+            //                    x.BirthDate
+            //                };
+            //dataGridView1.DataSource = employees.ToList();
+
+            //var employees = from x in db.Employees
+            //                where x.EmployeeID >= 2 && x.EmployeeID <= 8
+            //                orderby x.FirstName
+            //                select new
+            //                {
+            //                    x.FirstName,
+            //                    x.LastName,
+            //                    x.Title,
+            //                    x.BirthDate
+            //                };
+
+            //dataGridView1.DataSource = employees.ToList();
+
+
+            //var result = from emp in db.Employees
+            //             where SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) > 60
+            //             orderby SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) descending
+            //             select new
+            //             {
+            //                 Adı = emp.FirstName,
+            //                 Soyadı = emp.LastName,
+            //                 Yas = SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now)
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+
+            //var result = from emp in db.Employees
+            //             where emp.TitleOfCourtesy == "Mr." && SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) > 60
+            //             select new
+            //             {
+            //                 Adı = emp.FirstName,
+            //                 Soyadı = emp.LastName,
+            //                 Title = emp.TitleOfCourtesy,
+            //                 Yas = SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now)
+            //             };
+
+            //dataGridView1.DataSource = result.ToList();
+
+
+
+            //var result = from od in db.Order_Details
+            //             orderby od.Quantity * od.UnitPrice * (int)Math.Floor(1 - od.Discount)
+            //             group od by od.OrderID into kyw
+            //             select new
+            //             {
+            //                 OrderId = kyw.Key,
+            //                 Total = kyw.Sum(x=> x.Quantity * x.UnitPrice * (int)Math.Floor(1- x.Discount))
+            //             };
+
+            //dataGridView1.DataSource = result.ToList();
+
+
+
+            //dataGridView1.DataSource = db.Categories
+            //    .Join(db.Products, c => c.CategoryID, p => p.CategoryID, (c, p) => new { c, p })
+            //    .GroupBy(x => x.c.CategoryName)
+            //    .Select(x => new
+            //    {
+            //        CategoryName= x.Key,
+            //        Count = x.Sum(z=> z.p.UnitsInStock)
+            //    })
+            //    .OrderByDescending(x=> x.Count)
+            //    .ToList();
+
+            //dataGridView1.DataSource =
+            //    db.Products.Join(db.Order_Details, p => p.ProductID, od => od.ProductID, (p, od) => new { p, od })
+            //               .GroupBy(x => x.p.ProductName)
+            //               .Select(x => new
+            //               {
+            //                   UrunAdi = x.Key,
+            //                   Miktar = x.Sum(z => z.od.Quantity),
+            //                   Gelir = x.Sum(z => z.od.Quantity * z.od.UnitPrice * (int)Math.Floor(1 - z.od.Discount))
+            //               })
+            //               .OrderByDescending(x => x.Gelir)
+            //               .ToList();
+
+            dataGridView1.DataSource =
+            db.Employees.Join(db.Orders, emp => emp.EmployeeID, o => o.EmployeeID, (emp, o) => new { emp, o })
+                      .Join(db.Order_Details, ord => ord.o.OrderID, od => od.OrderID, (ord, od) => new { ord, od })
+                      .GroupBy(x => x.ord.emp.FirstName)
+                      .Select(x => new
+                      {
+                          Calisan = x.Key,
+                          Toplam = x.Sum(z => z.od.Quantity * z.od.UnitPrice * (int)Math.Floor(1 - z.od.Discount))
+                      })
+                      .OrderByDescending(x => x.Toplam)
+                      .ToList();
         }
     }
 }
