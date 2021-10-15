664bbb939024a3e547bc931377920ef03a399777
kadir-code <65715423+xkadir@users.noreply.github.com>
2021-10-16 00:16:40 +0300

    Stored Procedure Usage

---
+++ DbFirst_Procedure/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace DbFirst_Procedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Category :";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(314, 49);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategory.TabIndex = 5;
+            this.cmbCategory.SelectionChangeCommitted += new System.EventHandler(this.cmbCategory_SelectionChangeCommitted_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 273);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DbFirst_Procedure/Form1.cs
@@ -0,0 +1,36 @@
+﻿using DbFirst_Procedure.Models;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DbFirst_Procedure
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities1 db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new NorthwindEntities1();
+        }
+
+      
+
+        private void cmbCategory_SelectionChangeCommitted_1(object sender, EventArgs e)
+        {
+            Category id = cmbCategory.SelectedItem as Category;
+            if (id != null)
+                dataGridView1.DataSource = db.sp_getCategoryById(id.CategoryID);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCategory.DataSource = db.Categories.ToList();
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
+            Category id = cmbCategory.SelectedItem as Category;
+            if (id != null)
+                dataGridView1.DataSource = db.sp_getCategoryById(id.CategoryID);
+        }
+    }
+}
