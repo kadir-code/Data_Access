8a84992903699b5f656ddae2410b3b3ae26f7237
kadir-code <65715423+xkadir@users.noreply.github.com>
2021-10-17 00:49:48 +0300

    DapperOperations_Northwind

---
+++ DapperOperations_Northwind/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace DapperOperations_Northwind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExacute = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 216);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnExacute
+            // 
+            this.btnExacute.Location = new System.Drawing.Point(316, 302);
+            this.btnExacute.Name = "btnExacute";
+            this.btnExacute.Size = new System.Drawing.Size(102, 34);
+            this.btnExacute.TabIndex = 1;
+            this.btnExacute.Text = "Exacute";
+            this.btnExacute.UseVisualStyleBackColor = true;
+            this.btnExacute.Click += new System.EventHandler(this.btnExacute_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(294, 259);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(146, 22);
+            this.txtValue.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnExacute);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExacute;
+        private System.Windows.Forms.TextBox txtValue;
+    }
+}
+
---
+++ DapperOperations_Northwind/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+namespace DapperOperations_Northwind
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection db = new SqlConnection("Server=DESKTOP-J1PKMI5\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExacute_Click(object sender, EventArgs e)
+        {
+            //update by execute
+            //get list of employees by query
+            db.Execute("update employees set firstname='jane' where employeeId=1");
+            dataGridView1.DataSource = db.Query("select * from employees").ToList();
+
+            //delete using execute
+            db.Execute("delete from employees where employeeId=1");
+            dataGridView1.DataSource = db.Query("select * from employees").ToList();
+
+            //get top 5 elements
+            dataGridView1.DataSource = db.Query("select top 5 * from categories").ToList();
+
+            //strongly typed in case of having category class
+            dataGridView1.DataSource = db.Query<Category>("select top 5 * from categories").ToList();
+
+            //joined categories andproducts
+            string sql = "select * from categories as c inner join products p on c.categoryId=p.categoryId";
+            dataGridView1.DataSource = db.Query(sql).ToList();
+
+            //get 10 most expensive products using stored procedure
+            var sql = "[Ten Most Expensive Products]";
+            dataGridView1.DataSource = db.Query(sql, commandType: CommandType.StoredProcedure).ToList();
+
+            //get category bı user typed id from textbox using stored procedure
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@id", txtValue.Text.ToString());
+            dataGridView1.DataSource = db.Query("sp_ProductByCategory", param, commandType: CommandType.StoredProcedure).ToList();
+
+            //Write the sp that calculates how much shopping the customer, whose ID is given, has made in total
+            DynamicParameters param = new DynamicParameters();
+            param.Add("Id", txtValue.Text.ToString());
+            dataGridView1.DataSource = db.Query("sp_GetCustomerOrders", param, commandType: CommandType.StoredProcedure).ToList();
+        }
+
+       
+    }
+}
